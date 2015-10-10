f7a9ef203fac52acfe406a06193e26e2b54eefa3
Jesus Lopez de Nava <lodena@users.noreply.github.com>
2015-10-10 16:19:22 -0500

    Initial Commit

---
+++ Tapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tapper
+//
+//  Created by Jesus Lopez de Nava on 10/10/15.
+//  Copyright © 2015 LodenaApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
